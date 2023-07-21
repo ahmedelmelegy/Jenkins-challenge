d0a3d6967a7e2d7651e9dc91d31640dd2d453d61
ahmed elmelegy <62904201+ahmedelmelegy@users.noreply.github.com>
2023-07-21 21:14:17 +0300

    Update algorithm.sh

--- algorithm.sh
+++ algorithm.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-echo "MODIFY THE ALGORITHM TO PRODUCE A MORE PRECISE CALCULATION OF PI" | tee report.txt
-echo "3.14159" | tee -a report.txt
+echo "scale=1000; 4*a(1)" | bc -l | tee report.txt
